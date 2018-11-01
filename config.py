60c64cb805a37ee771192d25105920137d8d0d7c
Yiwen Xu <yiwen@Yiwens-MBP.wv.cc.cmu.edu>
2018-10-31 22:51:19 -0400

    update config.py and add save_model() on train.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 MODEL_CONFIG = {
     "batch_size": 2,
-    "nepochs": 2
+    "hidden_size": 256,
+    "nlayers": 3,
+    "lr": 1e-3,
 }
 
--- train.py
+++ train.py
@@ -27,27 +27,86 @@ def train(train_loader, model, optimizer, ctc, epoch):
         if i % 10 == 0:
             print("epoch {}, step {}, loss per step {}, finish {}".format(
                 epoch, i, loss_sum/10, (i+1)*len(inputs)))
-        if i % 100 == 0:
-            save_model()
+            loss_sum = 0
+        if i % MC["checkpoint"] == 0:
+            save_model(epoch, model, optimizer, loss.item(), i, "./weights")
 
- main():
 
+def save_model(epoch, model, optimizer, loss, step, save_path):
+    filename = save_path + str(epoch) + '-' + str(step) + '-' + "%.6f" % loss.item() + '.pth'
+    print('Save model at Train Epoch: {} [Step: {}\tLoss: {:.12f}]'.format(
+        epoch, step, loss.item()))
+    state = {
+        'epoch': epoch + 1,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'loss': loss
+    }
+    torch.save(state, filename)
+
+def load_model(epoch, step, loss, model, optimizer, save_path):
+    filename = save_path + str(epoch) + '-' + str(step) + '-' + str(loss) + '.pth'
+    if os.path.isfile(filename):
+        print("######### loading weights ##########")
+        checkpoint = torch.load(filename)
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        loss = checkpoint['loss']
+        print('########## loading weights done ##########')
+        return model, optimizer, start_epoch, loss
+    else:
+        print("no such file: ", filename)
+
+def main(args):
     train_path = "./data/wsj0_train.npy"
     label_path = "./data/wsj0_train_merged_labels.npy"
     train_dataset = myDataset(train_path, label_path)
     train_loader = DataLoader(train_dataset, batch_size=MC["batch_size"], shuffle=False, collate_fn=collate)
 
-    model = PackedLanguageModel(N_PHONEMES, 40, 256, 3)
+    model = PackedLanguageModel(N_PHONEMES, 40, MC["hidden_size"], MC["nlayers"])
     model = model.to(DEVICE)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-6)
+    optimizer = torch.optim.Adam(model.parameters(), lr=MC["lr"], weight_decay=1e-6)
     ctc = CTCLoss()
     start_epoch = 0
+    if args.resume is True:
+        model, optimizer, start_epoch, loss = load_model(
+            args.load_epoch,
+            args.load_step,
+            args.load_loss,
+            model,
+            optimizer,
+            args.weights_path
+        )
 
-    nepochs = MC["nepochs"]
+    nepochs = args.epochs
     for epoch in range(start_epoch, nepochs):
         model.train()
-        train(train_loader, model, optimizer, ctc)
+        train(train_loader, model, optimizer, ctc, epoch)
+
+def arguments(args):
+    parser = argparse.ArgumentParser(description="Speaker Verificiation via CNN")
+    parser.add_argument('--epochs', type=int, default=27, metavar='E',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--weight-decay', type=float, default=0.001,
+                        help='L2 regularization')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--checkpoint', type=int, default=50, metavar="R",
+                        help='checkpoint to save model parameters')
+    parser.add_argument('--resume', type=bool, default=False, metavar="R",
+                        help='resume training from saved weight')
+    parser.add_argument('--weights-path', type=str, default="./weights",
+                        help='path to save weights')
+    parser.add_argument('-load-epoch', type=str, default=0, metavar="LE",
+                        help='number of epoch to be loaded')
+    parser.add_argument('-load-step', type=str, default=0, metavar="LS",
+                        help='number of step to be loaded')
+    parser.add_argument('-load-loss', type=str, default=0, metavar="LL",
+                        help='loss item to be loaded')
+    return parser.parse_args()
 
 if __name__ == '__main__':
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    main()
+    args = arguments()
+    main(args)
